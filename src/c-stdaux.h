6549fa0eb8f3bda1ab8f37f78702c8cda3fd83ee
David Rheinsberg <david.rheinsberg@gmail.com>
2022-10-25 14:36:03 +0200

    c-stdaux: extract unix'ish code into separate module
    Move all the unix'ish code into a separate module called
    c-stdaux-unix.h, thus aiding portability to non-unix platforms and
    allowing use on platforms without close(2), unistd.h, etc.
    
    Signed-off-by: David Rheinsberg <david.rheinsberg@gmail.com>

--- src/c-stdaux.h
+++ src/c-stdaux.h
@@ -42,33 +42,9 @@ extern "C" {
 
 #include <c-stdaux-generic.h>
 
-/**
- * DOC: Guaranteed Includes
- *
- * The ``c-stdaux.h`` header includes a set of C Standard Library headers as
- * well as UNIX headers. All those includes are guaranteed and part of the API.
- * See the actual header for a comprehensive list.
- */
-/**/
-
-#include <assert.h>
-#include <dirent.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <inttypes.h>
-#include <limits.h>
-#include <stdalign.h>
-#include <stdarg.h>
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdnoreturn.h>
-#include <string.h>
-#include <sys/time.h>
-#include <sys/types.h>
-#include <time.h>
-#include <unistd.h>
+#if defined(C_OS_LINUX) || defined(C_OS_MACOS)
+#  include <c-stdaux-unix.h>
+#endif
 
 /**
  * DOC: Compiler Attributes
@@ -497,73 +473,6 @@ static inline void *c_internal_container_of(void *ptr, size_t offset) {
  */
 #define c_align_to(_val, _to) C_CC_MACRO2(C_ALIGN_TO, (_val), (_to))
 #define C_ALIGN_TO(_val, _to) (((_val) + (_to) - 1) & ~((_to) - 1))
-
-/**
- * DOC: Common Destructors
- *
- * A set of destructors is provided which extends standard library destructors
- * to adhere to some adjuvant rules. In particular, they return an invalid
- * value of the particular object, rather than void. This allows direct
- * assignment to any member-field and/or variable they are defined in, like:
- *
- * .. code-block:: c
- *
- *     foo = c_free(foo);
- *     foo->bar = c_close(foo->bar);
- *
- * Furthermore, all those destructors can be safely called with the "INVALID"
- * value as argument, and they will be a no-op.
- */
-/**/
-
-/**
- * c_close() - Destructor-wrapper for close()
- * @fd:         File-descriptor to pass to destructor, or negative value
- *
- * Wrapper around ``close()``, but a no-op if a negative value is provided.
- * Always returns ``-1``.
- *
- * Return: -1 is returned.
- */
-static inline int c_close(int fd) {
-        if (fd >= 0)
-                close(fd);
-        return -1;
-}
-
-/**
- * c_closedir() - Destructor-wrapper for closedir()
- * @d:          Directory handle to pass to destructor, or NULL
- *
- * Wrapper around ``closedir()``, but a no-op if ``NULL`` is passed. Always
- * returns ``NULL``.
- *
- * Return: NULL is returned.
- */
-static inline DIR *c_closedir(DIR *d) {
-        if (d)
-                closedir(d);
-        return NULL;
-}
-
-/**
- * DOC: Common Cleanup Helpers
- *
- * A set of helpers that aid in creating functions suitable for use with
- * :c:macro:`_c_cleanup_()`. Furthermore, a collection of predefined cleanup
- * functions of a set of standard library objects ready for use with
- * :c:macro:`_c_cleanup_()`.
- * Those cleanup helpers are always suffixed with a ``p``.
- *
- * The helpers that are provided are:
- *
- * - ``c_closep()``: Wrapper around :c:func:`c_close()`.
- * - ``c_closedirp()``: Wrapper around :c:func:`c_closedir()`.
- */
-/**/
-
-C_DEFINE_DIRECT_CLEANUP(int, c_close);
-C_DEFINE_CLEANUP(DIR *, c_closedir);
 
 #ifdef __cplusplus
 }
